67412c7f7eafd0a3d6e82de9ab8a1e3c203c865d
Darren L <darren.lou@gmail.com>
2019-03-24 02:07:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,10 @@
 class Dog
   
+  attr_accessor :name, :breed
+  
   def initialize(name, breed)
       @breed = breed
       @name = name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
   end
  
 end
